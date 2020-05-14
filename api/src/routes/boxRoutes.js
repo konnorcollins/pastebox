384edc90dc3d25a394a8baaff26e4ce826e66c56
Konnor Collins <konnor.s.collins@gmail.com>
2020-05-13 20:53:06 -0600

    Added body-parser for POST /box route

--- api/src/routes/boxRoutes.js
+++ api/src/routes/boxRoutes.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 // Box Schema
 const Box = require('../models/Box');
@@ -10,16 +11,25 @@ const Box = require('../models/Box');
 // POST /box
 router.route('')
     .post(async (req, res) => {
+        const text = req.body.text; // text contents
+        // TODO: generate box with text, insert and save
+        // TODO: respond with id
     });
 
 // GET /box/:id
 router.route('/:id')
     .get(async (req, res) => {
+        const id = req.params.id;
+        // TODO: fetch box based on id
+        // TODO: send box data
     });
 
 // DEL /box/:id
 router.route('/:id')
     .delete(async (req, res) => {
+        const id = req.params.id;
+        // TODO: delete box based on id
+        // TODO: send confirmation response of some sort
     });
 
 
