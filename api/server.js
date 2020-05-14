e1c8bc0a33718350d745e54e8a2fdf13af5b799b
Konnor Collins <konnor.s.collins@gmail.com>
2020-05-13 21:06:29 -0600

    Added cors.

--- api/server.js
+++ api/server.js
@@ -3,12 +3,24 @@
 // dependencies
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// TODO: mongo db connection
 
 // constants
 const port = process.env.PORT || 8080;
+const boxPath = '/box';
 
-// app
+// routes
+const boxRoutes = require('./src/routes/boxRoutes');
+
+// application
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(boxPath, boxRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
