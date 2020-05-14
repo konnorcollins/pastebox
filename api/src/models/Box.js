394528be7efcbda09c83b08013b21e1a6de80b6c
Konnor Collins <konnor.s.collins@gmail.com>
2020-05-13 20:13:28 -0600

    Fixed box schema definition.

--- api/src/models/Box.js
+++ api/src/models/Box.js
@@ -2,11 +2,19 @@
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { nanoid } = require('nanoid');
 
 const Box = new Schema(
     {
-        _id: String,
-        filename: String
+        _id: 
+        {
+            type: String,
+            default: () => nanoid()
+        },
+        filename:
+        {
+            type: String
+        }
     }
     ,{
         collection: 'Boxes' 
